d45c6ab4dc4cf4ea323e96850862ef84f0ee0bc3
ThoamsLyu <861480637@qq.com>
2017-07-21 09:54:40 +0800

    Note Taking APP(Make school style)
    First time try this and it will be the first review material for me

---
+++ MakeSchoolNotes-Swift-V2-Starter-swift3-coredata/MakeSchoolNotes/Helpers/CoreDataHelper.swift
@@ -0,0 +1,44 @@
+//
+//  CoreDataHelper.swift
+//  MakeSchoolNotes
+//
+//  Created by 吕东昇 on 2017/7/20.
+//  Copyright © 2017年 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+
+class CoreDataHelper {
+    static let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    static let persistentContainer = appDelegate.persistentContainer
+    static let managedContext = persistentContainer.viewContext
+    //static methods will go here
+    static func newNote() -> Note{
+        let note = NSEntityDescription.insertNewObject(forEntityName: "Note", into: managedContext)as! Note
+        return note
+    }
+    static func saveNote(){
+        do{
+            try managedContext.save()
+        }catch let error as NSError{
+            print("Could not save \(error)")
+        }
+    }
+    static func delete(note: Note){
+        managedContext.delete(note)
+        saveNote()
+    }
+    static func retrieveNotes()->[Note]{
+        let fetchRequest = NSFetchRequest<Note>(entityName:"Note")
+        do{
+            let results = try managedContext.fetch(fetchRequest)
+            return results.reversed()
+        }catch let error as NSError{
+            print("Could not save \(error)")
+        }
+        return []
+    }
+}
---
+++ MakeSchoolNotes-Swift-V2-Starter-swift3-coredata/MakeSchoolNotes/Models/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  MakeSchoolNotes
+//
+//  Created by 吕东昇 on 2017/7/20.
+//  Copyright © 2017年 MakeSchool. All rights reserved.
+//
+
+import Foundation
+class Note {
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
